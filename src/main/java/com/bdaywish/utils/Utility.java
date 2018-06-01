c242cbbba12c0ff8c02884f6485ea2b0ad881a38
ATMECS\Abhijit.Jadhav <abhijit.jadhav@atmecs.com>
2018-06-01 19:16:17 +0530

    Updated mail functionality

--- src/main/java/com/bdaywish/utils/Utility.java
+++ src/main/java/com/bdaywish/utils/Utility.java
@@ -1,25 +1,39 @@
 package com.bdaywish.utils;
 
 
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.MalformedURLException;
 import java.text.SimpleDateFormat;
 import java.util.*;
+
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
+import javax.imageio.ImageIO;
 import javax.mail.*;
 import javax.mail.Message;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+import org.apache.commons.lang3.StringUtils;
 
 
 public class Utility {
 
 	public String sendMail(String name,String toMail, String subject, String message) {
-		//imageUrl = "https://www.birthdaywishes.expert/wp-content/uploads/2015/06/Happy-birthday-wish-cup-cake-candles-balloons.jpg";
+		String imagePath = "./src/main/resources/hbdayWithMessage.jpg";
 		final String SSL_FACTORY = "javax.net.ssl.SSLSocketFactory";
 		Properties props = System.getProperties();
 		props.setProperty("mail.smtp.host", "smtp.gmail.com");
@@ -28,7 +42,7 @@ public class Utility {
 		props.setProperty("mail.smtp.port", "465");
 		props.setProperty("mail.smtp.socketFactory.port", "465");
 		props.put("mail.smtp.auth", "true");
-		props.put("mail.debug", "true");
+		props.put("mail.debug", "false");
 		props.put("mail.store.protocol", "pop3");
 		props.put("mail.transport.protocol", "smtp");
 
@@ -42,23 +56,35 @@ public class Utility {
 					}
 				});
 				Message msg = new MimeMessage(session);
-				/*try {
-					BufferedImage image = ImageIO.read(new URL(imageUrl));
-					Graphics g = image.getGraphics();
-					g.setFont(g.getFont().deriveFont(30f));
-					g.drawString("Hello World!", 100, 100);
-					g.dispose();
-					ImageIO.write(image, "jpg", new File(
-							"C:/Users/Public/Pictures/image.jpg"));
-
-				} catch (IOException e) {
-					e.printStackTrace();
-				}*/
 				msg.setFrom(new InternetAddress(username));
 				msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toMail, false));
 				msg.setSubject(subject);
-				msg.setContent("<H4> Hi! " + name + " </H4><H2>"+message+"</H2><BR/>" + "" + "<IMG SRC='https://www.birthdaywishes.expert/wp-content/uploads/2015/06/Happy-birthday-wish-cup-cake-candles-balloons.jpg'/>", "text/html" );
+				String finalMessage = "Hi "+name +"!!! \n" +message;
+				writeMessageOnImage(finalMessage);
+				//+ "" + "<IMG SRC='"+imageFile.toURI().toURL()+"'/>"
+				//msg.setContent("<H4> Hi! " + name + " </H4><H2>"+message+"</H2><BR/>" , "text/html" );
+				MimeMultipart multipart = new MimeMultipart("related");
+				//msg.setDataHandler(new DataHandler(new FileDataSource(imagePath)));
 				msg.setSentDate(new Date());
+				BodyPart messageBodyPart = new MimeBodyPart();
+				String htmlText = "Hi "+name +" !!! <img src=\"cid:image\">";
+				messageBodyPart.setContent(htmlText, "text/html");
+				// add it
+				multipart.addBodyPart(messageBodyPart);
+
+				// second part (the image)
+				messageBodyPart = new MimeBodyPart();
+				DataSource fds = new FileDataSource(
+						imagePath);
+
+				messageBodyPart.setDataHandler(new DataHandler(fds));
+				messageBodyPart.setHeader("Content-ID", "<image>");
+
+				// add image to the multipart
+				multipart.addBodyPart(messageBodyPart);
+
+				// put everything together
+				msg.setContent(multipart);
 				Transport.send(msg);
 				System.out.println("Message sent.");
 				return "success";
@@ -67,6 +93,26 @@ public class Utility {
 		} catch (MessagingException e) {
 			System.out.println("Error, cause: " + e);
 			return "Cant send mail";
+		}
+	}
+
+	private void writeMessageOnImage(String message) {
+		try {
+			String sal = StringUtils.substringBefore(message, StringUtils.lowerCase("\n"));
+			String msg = StringUtils.substringAfter(message, StringUtils.lowerCase("\n"));
+			BufferedImage bufferedImage = ImageIO.read(new File("./src/main/resources/hbday.jpg"));
+			Graphics graphics = bufferedImage.getGraphics();
+			// graphics.fillRect(20, 30, 200, 50);
+			graphics.setColor(Color.BLACK);
+			graphics.setFont(new Font("Arial Black", Font.BOLD, 20));
+			graphics.drawString(sal, 70, 35);
+			graphics.drawString(msg, 80, 55);
+			
+			ImageIO.write(bufferedImage, "jpg", new File(
+					"./src/main/resources/hbdayWithMessage.jpg"));
+
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 
@@ -81,7 +127,6 @@ public class Utility {
 			System.out.println("You are in catch block -- Exception Occurred for: " + email);
 		}
 		return isValid;
-
 	}
 
 
@@ -91,7 +136,7 @@ public class Utility {
 		//System.out.println(dateString);
 		Date date = new Date (dateString);
 		//System.out.println(date);
-	//	System.out.println(date.getTime());
+		//	System.out.println(date.getTime());
 		return date.getTime()/1000;
 	}
 
